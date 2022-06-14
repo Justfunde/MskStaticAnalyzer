d1782b9404e9cc3eb9b94d07de47a56792e05863
Mikhail Kotlyarov <Misha2104201@gmail.com>
2022-06-14 23:30:37 +0300

    Добавлена оброботка передачи параметров через консоль

--- MskStaticAnalyzer/main.cpp
+++ MskStaticAnalyzer/main.cpp
@@ -1,54 +1,58 @@
 #include "LayoutReader_MSK.hpp"
 #include "MskStaticAnalyzer.hpp"
+#include <array>
 
-//#include "NewPrettyTable.hpp"
+constexpr uint32_t g_maxConsoleParamCnt = 4;
+constexpr uint32_t g_MskFileNameInd = 1;
+constexpr uint32_t g_ruleFileNameInd = 2;
+constexpr uint32_t g_outputFileName = 3;
+
+
+std::vector<std::string>
+SplitConsoleStrings(
+   int argc,
+   char* argv[])
+{
+  
+   std::vector<std::string> strings(argc);
+
+   for (uint8_t i = 0; i < argc; i++)
+   {
+      strings.push_back(argv[i]);
+   }
+   return strings;
+}
+
 
 int main(int argc, char* argv[]) {
   
+   try
+   {
+      if (1 == argc || g_maxConsoleParamCnt < argc) { throw std::runtime_error("Invalid number of parameters"); }
+
+      auto consoleData = SplitConsoleStrings(argc, argv);
+
+      RuleParser ruleParser(consoleData.at(g_ruleFileNameInd));
+      if (!ruleParser.ParseFile()) { throw std::runtime_error("Rule file was not parsed!"); }
 
 
-
-   MskStaticAnalyzer analyzer;
-   RuleParser parser("tests/cmos012.rul");
-   parser.ParseFile();
-   LayoutData  layoutFirst;
-   // //freopen("before.txt", "w", stdout);
-   // //std::wstring FileName = L"tests/inv.gds";
-   // //std::wstring FileName = L"output.gds";
-   // //std::wstring FileName = L"tests/nand2.gds";
-   // //std::wstring FileName = L"tests/xor.gds";
-   // //std::wstring FileName = L"tests/1Kpolyg.gds";
-   // //std::wstring FileName = L"tests/testDesign.gds";
-    std::wstring fileNameFirst = L"tests/Kotlyarov LR4.msk";
-
-    LayoutReader* p_readerFirst = GetReader(fileNameFirst);
-    if (!p_readerFirst) {
-        std::wcerr << "__err__ : Can't file appropriate reader for given file '" << fileNameFirst << "'." << std::endl;
-        return EXIT_FAILURE;
-    }
-    if (!p_readerFirst->Read(&layoutFirst)) {
-        std::wcerr << "__err__ : Can't read file '" << fileNameFirst << "'." << std::endl;
-        FreeReader(p_readerFirst);
-        return EXIT_FAILURE;
-    }
-    FreeReader(p_readerFirst);
-    analyzer.SetParameters(&layoutFirst, parser.GetRules());
-    analyzer.WriteAnalyzedFile("tests/testAnalyzerFile.txt");
-
-   //// std::wcout << "Input file " << fileNameFirst << " has " << layoutFirst.libraries.size() << " library(ies)" << std::endl;
-   ////
-   //// for (size_t i = 0; i < layoutFirst.libraries.size(); ++i) {
-   ////    std::cout << "  - Library [" << i << "] has name '" << layoutFirst.libraries[i]->name << "' and contains " << layoutFirst.libraries[i]->elements.size() << " elements:" << std::endl;
-   ////    for (size_t j = 0; j < layoutFirst.libraries[i]->elements.size(); ++j)
-   ////        std::cout << "      * " << layoutFirst.libraries[i]->elements[j]->name << " (contains " << layoutFirst.libraries[i]->elements[j]->geometries.size() << " geometries)" << std::endl;
-   ////    std::cout << "    Library [" << i << "] also contains " << layoutFirst.libraries[i]->layers.size() << " layers (in order of appearance):" << std::endl;
-   ////    std::cout << "      { ";
-   ////    for (size_t j = 0; j < layoutFirst.libraries[i]->layers.size(); ++j)
-   ////        std::cout << layoutFirst.libraries[i]->layers[j].layer << " ";
-   ////    std::cout << " }" << std::endl;
-   ////}
+      LayoutData layoutData;
 
 
+      LayoutReader* layoutReader = GetReader(std::wstring(consoleData.at(g_MskFileNameInd).begin(), consoleData.at(g_MskFileNameInd).end()));
+      if (!layoutReader) { throw std::runtime_error("Can't file appropriate reader for given file"); }
+      if (!layoutReader->Read(&layoutData)) { throw std::runtime_error("Can't read file"); }
+      FreeReader(layoutReader);
 
-    return 0;
+      MskStaticAnalyzer analyzer;
+
+      analyzer.SetParameters(&layoutData, ruleParser.GetRules());
+      if (!analyzer.WriteAnalyzedFile(consoleData.at(g_outputFileName))) { throw std::runtime_error("Can't write analyzed file!");  }
+   }
+   catch (const std::exception& ex)
+   {
+      std::cerr << "Exteption:" << ex.what() << std::endl;
+      return EXIT_FAILURE;
+   }
+   return 0;
 }
