cd0c18a4c80dbe14a1e2fa4da313faeeadda0b5c
Mikhail Kotlyarov <Misha2104201@gmail.com>
2022-06-15 00:41:58 +0300

    Финальные правки

--- MskStaticAnalyzer/main.cpp
+++ MskStaticAnalyzer/main.cpp
@@ -1,11 +1,11 @@
 #include "LayoutReader_MSK.hpp"
 #include "MskStaticAnalyzer.hpp"
-#include <array>
 
-constexpr uint32_t g_maxConsoleParamCnt = 4;
-constexpr uint32_t g_MskFileNameInd = 1;
-constexpr uint32_t g_ruleFileNameInd = 2;
-constexpr uint32_t g_outputFileName = 3;
+constexpr uint32_t g_maxConsoleParamCnt = 5;
+constexpr uint32_t g_CheckoutParam = 1;
+constexpr uint32_t g_MskFileNameInd = 2;
+constexpr uint32_t g_ruleFileNameInd = 3;
+constexpr uint32_t g_outputFileName = 4;
 
 
 std::vector<std::string>
@@ -14,7 +14,7 @@ SplitConsoleStrings(
    char* argv[])
 {
   
-   std::vector<std::string> strings(argc);
+   std::vector<std::string> strings;
 
    for (uint8_t i = 0; i < argc; i++)
    {
@@ -28,13 +28,16 @@ int main(int argc, char* argv[]) {
   
    try
    {
-      if (1 == argc || g_maxConsoleParamCnt < argc) { throw std::runtime_error("Invalid number of parameters"); }
+     if (1 == argc || g_maxConsoleParamCnt < argc) { throw std::runtime_error("Invalid number of parameters"); }
 
       auto consoleData = SplitConsoleStrings(argc, argv);
+
+      if("--proc" != consoleData.at(g_CheckoutParam)) { throw std::runtime_error("Invalid console param"); }
 
       RuleParser ruleParser(consoleData.at(g_ruleFileNameInd));
       if (!ruleParser.ParseFile()) { throw std::runtime_error("Rule file was not parsed!"); }
 
+      std::cout << "\nRule was parced well!\n";
 
       LayoutData layoutData;
 
@@ -44,14 +47,18 @@ int main(int argc, char* argv[]) {
       if (!layoutReader->Read(&layoutData)) { throw std::runtime_error("Can't read file"); }
       FreeReader(layoutReader);
 
+      std::cout << "File was read correct!\n";
+
       MskStaticAnalyzer analyzer;
 
       analyzer.SetParameters(&layoutData, ruleParser.GetRules());
       if (!analyzer.WriteAnalyzedFile(consoleData.at(g_outputFileName))) { throw std::runtime_error("Can't write analyzed file!");  }
+      std::cout << "Output file is constructed!\n";
+
    }
    catch (const std::exception& ex)
    {
-      std::cerr << "Exteption:" << ex.what() << std::endl;
+      std::cerr << "\nExteption:" << ex.what() << std::endl;
       return EXIT_FAILURE;
    }
    return 0;
